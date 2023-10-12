347f630fab7df6414ac1e8678d75e7e9957527fa
Andrew Zhang <99509483+zhangandrew2@users.noreply.github.com>
2023-10-12 02:45:18 -0700

    problem 121

---
+++ 121BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int currlowest = prices[0];
+        for (int i =1;i<prices.size();i++){
+            if (prices[i] < currlowest)
+                currlowest = prices[i];
+            if(prices[i]-currlowest > max)
+                max = prices[i]-currlowest;
+        }
+        return max;
+    }
+};
+
+int main(){
+    int test[] = {7,1,5,3,6,4};
+    vector<int> prices = test;
+    Solution tester;
+    cout << "profits: " << tester.maxProfit(&prices);
+
+}
